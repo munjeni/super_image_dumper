daae1a7dec6a630369a51f6e0d86ba3a2545d5ff
munjeni <sharecard@gmx.com>
2021-04-03 10:01:43 -0700

    update pie makefile

--- Android.mk
+++ Android.mk
@@ -3,6 +3,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := superunpack.arm64_pie
 LOCAL_CFLAGS += -Iinclude
-LOCAL_SRC_FILES := superunpack.c
+LOCAL_SRC_FILES := superunpack.c version.h
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := superrepack.arm64_pie
+LOCAL_CFLAGS += -Iinclude
+LOCAL_SRC_FILES := superrepack.c version.h
 
 include $(BUILD_EXECUTABLE)
